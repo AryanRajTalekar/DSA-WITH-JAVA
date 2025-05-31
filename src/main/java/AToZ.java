925e11a9d2246b70fa3d80ad9f30f134ca45fefe
AryanRajTalekar <aryanrajtalekar@gmail.com>
2025-05-31 21:05:43 +0530

    Print A to Z

--- src/main/java/AToZ.java
+++ src/main/java/AToZ.java
@@ -1,2 +1,14 @@
 public class AToZ {
+
+    public static void main(String args[]){
+
+        StringBuilder rs = new StringBuilder();
+
+        for(int i=0;i<26;i++){
+            char res = (char)('A'+i);
+            rs.append(res + " ");
+        }
+
+        System.out.println(rs.toString());
+    }
 }
